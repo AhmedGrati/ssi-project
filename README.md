51c1aa1bc8bf0077a66b237175b979a77926f51d
ahmed <ahmedgrati1999@gmail.com>
2021-12-10 21:50:41 +0100

    Add env config

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # ssi-project
+Before running the project please run the script in the ```dep.sh``` file.<br>
+Then you should create an ```.env``` file from the ```.example.env``` file.<br>
+To have a proper execution, please run it using this command:
+```shell
+python3 main.py
+```
--- main.py
+++ main.py
@@ -1,13 +1,22 @@
+import os
+
 from db_connector import DBConnector
 from repository import UserRepository
 import auth
+from dotenv import load_dotenv
+
+load_dotenv(".env")
 if __name__ == '__main__':
     user_choice = None
+    DB_HOSTNAME: str = os.environ.get("DB_HOSTNAME")
+    DB_USER: str = os.environ.get("DB_USER")
+    DB_PASSWORD: str = os.environ.get("DB_PASSWORD")
+    DB_DATABASE: str = os.environ.get("DB_DATABASE")
     conn = DBConnector.Instance(
-        hostname="localhost",
-        user="ahmed",
-        password="ahmed",
-        database="ssi"
+        hostname=DB_HOSTNAME,
+        user=DB_USER,
+        password=DB_PASSWORD,
+        database=DB_DATABASE
     )
     user_repo = UserRepository(db_connector=conn)
     while user_choice != 4:
