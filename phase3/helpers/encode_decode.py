0392feeef7514eb0521f43c7c57fbce0de2e6e84
wadhah <wadhah.mahroug15@gmail.com>
2021-12-18 19:40:23 +0100

    completed 1

--- phase3/helpers/encode_decode.py
+++ phase3/helpers/encode_decode.py
@@ -1,18 +1,20 @@
-from cryptography.fernet import Fernet
 from phase3.helpers.generic_cli import generic_cli
 
 
-def encryp_fernet():
-    print("")
+def encode_bytes():
+    message = input("Enter Message to be encoded into Bytes: ")
+    print(f'Encoded Message:\n{message.encode("utf-8").hex()}\n')
 
 
-def decrypt_fernet():
-    print("")
+def decode_bytes():
+    message = input("Enter Message to be decoded from Bytes: ")
+    word = bytes.fromhex(message).decode("utf-8")
+    print(f'Decoded Message:\n{word}\n')
 
 
 menu = {
-    "a": {"message":  "Enocde Message", "func": encryp_fernet},
-    "b": {"message": "Decode Message", "func": decrypt_fernet},
+    "a": {"message":  "Enocde Message", "func": encode_bytes},
+    "b": {"message": "Decode Message", "func": decode_bytes},
 }
 
 
