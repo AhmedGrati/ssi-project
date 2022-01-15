55ae0d3796d1f747a57daecd9b0ae09c796a3a54
wadhah <wadhah.mahroug15@gmail.com>
2022-01-15 13:56:19 +0100

    added el gamal

--- phase3/helpers/encrypt_asym.py
+++ phase3/helpers/encrypt_asym.py
@@ -1,8 +1,13 @@
 import rsa
 
+from elgamal import elgamal
 from phase3.helpers.generic_cli import generic_cli
 
-pubkey, privkey = rsa.newkeys(512)
+rsa_pubkey, rsa_privkey = rsa.newkeys(512)
+
+_elgamal_keys = elgamal.generate_keys(128)
+elgamal_pubkey = _elgamal_keys["publicKey"]
+elgamal_privkey = _elgamal_keys["privateKey"]
 
 
 def pad(text: str, modulo: int):
@@ -15,14 +20,21 @@ def encrypt_rsa(key, message: str) -> str:
     return crypto.hex()
 
 
+def encrypt_elgamal(key, message: str) -> str:
+    crypto = elgamal.encrypt(key, message)
+    return crypto
+
+
 def encrypt_rsa_cli():
     message = input("Enter Message to be encrypted RSA: ")
-    enc_message = encrypt_rsa(key=pubkey, message=message)
-    print(f"Encrypted Message:\n{enc_message}\n")
+    enc_message = encrypt_rsa(key=rsa_pubkey, message=message)
+    print(f"Encrypted Message RSA:\n{enc_message}\n")
 
 
-def encrypt_Elgamal_cli():
-    return
+def encrypt_elgamal_cli():
+    message = input("Enter Message to be encrypted RSA: ")
+    enc_message = encrypt_elgamal(key=elgamal_pubkey, message=message)
+    print(f"Encrypted Message Elgamal:\n{enc_message}\n")
 
 
 def decrypt_rsa(key, message: str) -> str:
@@ -30,21 +42,27 @@ def decrypt_rsa(key, message: str) -> str:
     return crypto.decode("utf-8").strip()
 
 
-def decrypt_ras_cli():
+def decrypt_rsa_cli():
     message = input("Enter Message to be decrypted RSA: ")
-    dec_message = decrypt_rsa(key=privkey, message=message)
-    print(f"Encrypted Message:\n{dec_message}\n")
-    return
+    dec_message = decrypt_rsa(key=rsa_privkey, message=message)
+    print(f"Decrypted RSA Message:\n{dec_message}\n")
+
+
+def decrypt_elgamal(key, message: str) -> str:
+    crypto = elgamal.decrypt(key=key, cipher=message)
+    return crypto
 
 
 def decrypt_Elgamal_cli():
-    return
+    message = input("Enter Message to be decrypted RSA: ")
+    dec_message = decrypt_elgamal(key=elgamal_privkey, message=message)
+    print(f"Decrypted Elgamal Message:\n{dec_message}\n")
 
 
 menu = {
     "a": {"message": "Encrypt Message RSA", "func": encrypt_rsa_cli},
-    "b": {"message": "Encrypt Message Elgamal", "func": encrypt_Elgamal_cli},
-    "c": {"message": "Decrypt Message RSA", "func": decrypt_ras_cli},
+    "b": {"message": "Encrypt Message Elgamal", "func": encrypt_elgamal_cli},
+    "c": {"message": "Decrypt Message RSA", "func": decrypt_rsa_cli},
     "d": {"message": "Decrypt Message Elgamal", "func": decrypt_Elgamal_cli},
 }
 
